1a7273e62215bd65bbd353d924791a71eaf44a44
VaspulaVijayaLakshmi <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2021-09-14 20:36:08 +0530

    Update RequestRepository.java

--- supporthub.-main/supporthub5/src/main/java/com/example/supporthub5/repository/RequestRepository.java
+++ supporthub.-main/supporthub5/src/main/java/com/example/supporthub5/repository/RequestRepository.java
@@ -18,15 +18,19 @@ import java.util.Optional;
 
 @Repository
 public interface RequestRepository  extends JpaRepository<Request,Long> {
+    
     Optional<List<Request>> findByUser(User user);
+   
     Optional<Request> findByRequestId(Long id);
 
-
-
-
-    @Query(nativeQuery=true,value="SELECT  * from requests  WHERE (request_id=?1 or ?1 is null) AND (location_id=?2 or ?2 is null ) AND  (service_id=?3 or ?3 is null)  AND (description=?4 or ?4 is null )  AND  (state=?5 or ?5 is null) AND(created_on=?6 or ?6 is null)")
+    @Query(nativeQuery=true,value="SELECT  * from requests  WHERE (request_id=?1 or ?1 is null)    AND 
+                                                                  (location_id=?2 or ?2 is null)   AND
+                                                                  (service_id=?3 or ?3 is null)    AND 
+                                                                  (description=?4 or ?4 is null )  AND  
+                                                                  (state=?5 or ?5 is null)         AND
+                                                                 (created_on=?6 or ?6 is null) ")
     Optional<List<Request>> applyFilters ( Long requestId, Long
-            locationId, Long ServiceId, String description, String state,String createdOn);
+                                           locationId, Long ServiceId, String description, String state,String createdOn);
 
 
 }
