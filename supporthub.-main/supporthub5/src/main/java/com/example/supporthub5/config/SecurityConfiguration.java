d47137bd29505ce4cc8688ccb7c3bde687e407a7
VaspulaVijayaLakshmi <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2021-10-11 12:47:15 +0530

    Update SecurityConfiguration.java

--- supporthub.-main/supporthub5/src/main/java/com/example/supporthub5/config/SecurityConfiguration.java
+++ supporthub.-main/supporthub5/src/main/java/com/example/supporthub5/config/SecurityConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
-@EnableWebSecurity
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
